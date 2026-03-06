69a4ec4cd326ca6b88beaa705dee3e3d19ae12ca
Baishakhi Tikader <baitikader1@gmail.com>
2026-03-06 12:00:07 +0530

    Update probability_map.m

--- src/probability_map.m
+++ src/probability_map.m
@@ -1,32 +1,38 @@
-function probability_map(inputFile, outputFile, colX, colY, nBins, cutoffs, weights)
+function generate_probability_map(inputFile,nmax,cutoffs,weights)
 
-clc;
 format long
 
-data = load(inputFile);
+fileID = fopen(inputFile,'r');
+fileID2 = fopen('probability.out','w');
 
-x = data(:,colX);
-y = data(:,colY);
+formatSpec = '%f %f %f %f';
+sizeA = [5 Inf];
 
-range = [max(x) min(x) max(y) min(y)];
+P1 = fscanf(fileID,formatSpec,sizeA);
+fclose(fileID);
 
-dx = (range(1)-range(2))/nBins;
-dy = (range(3)-range(4))/nBins;
+P1 = P1';
+b = size(P1);
 
-p_g = ones(nBins+1,nBins+1)*1e-6;
-p_total = 0;
+range=[max(P1(:,2)) min(P1(:,2)) max(P1(:,3)) min(P1(:,3))];
 
-for i=1:length(x)
+dm=(range(1)-range(2))/nmax;
+dmu=(range(3)-range(4))/nmax;
 
-    for k = 1:size(cutoffs,1)
+p_g = ones(nmax+1,nmax+1)*1e-6;
+p_total=0;
 
-        if x(i) > cutoffs(k,1) && x(i) < cutoffs(k,2)
+for i=1:b(1)
 
-            ix = fix((x(i)-range(2))/dx)+1;
-            iy = fix((y(i)-range(4))/dy)+1;
+    for k=1:size(cutoffs,1)
 
-            p_g(ix,iy) = p_g(ix,iy) + weights(k);
-            p_total = p_total + weights(k);
+        if P1(i,2) > cutoffs(k,1) && P1(i,2) < cutoffs(k,2)
+
+            m = fix((P1(i,2)-range(2))/dm)+1;
+            mu = fix((P1(i,3)-range(4))/dmu)+1;
+
+            p_g(m,mu)=p_g(m,mu)+weights(k);
+            p_total=p_total+weights(k);
 
         end
 
@@ -34,21 +40,41 @@ for i=1:length(x)
 
 end
 
-p_g = p_g/p_total;
+p_g=p_g/p_total;
 
-xyz = dlmread(outputFile);
+m=range(2);
 
-x = xyz(:,1);
-y = xyz(:,2);
-z = -log(xyz(:,3));
+for i=1:nmax
+    m=m+dm;
+    mu=range(4);
 
-[X,Y] = meshgrid(min(x):dx:max(x),min(y):dy:max(y));
-Z = griddata(x,y,z,X,Y);
+    for j=1:nmax
+        mu=mu+dmu;
 
-mymap = [
-0.27 0.25 0.43;
-0.58 0.12 0.71;
-0.18 0.72 0.11;
+        if p_g(i,j)>0.00001
+            fprintf(fileID2,'%20.10f %20.10f %20.10f\n',m,mu,p_g(i,j));
+        end
+
+        fprintf(fileID2,'\n');
+
+    end
+end
+
+fclose(fileID2);
+
+xyz=dlmread('probability.out');
+
+x=xyz(:,1);
+y=xyz(:,2);
+z=-log(xyz(:,3));
+
+[X,Y]=meshgrid(min(x):dm:max(x),min(y):dmu:max(y));
+Z=griddata(x,y,z,X,Y);
+
+mymap=[
+0.27 0.25 0.43
+0.58 0.12 0.71
+0.18 0.72 0.11
 0.9 0.82 0.71
 ];
 
